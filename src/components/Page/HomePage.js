1e05c2bbd6cba8b019df70a1176e29548087712d
viweu <153850831+viweu@users.noreply.github.com>
2025-03-26 16:07:10 +0300

    Create HomePage.js

---
+++ src/components/Page/HomePage.js
@@ -0,0 +1,14 @@
+
+
+function HomePage() {
+    return ( 
+        <>
+            <h1>Главная страница</h1>
+            <h2>HomeWork 6</h2>
+            <h3>Фреймворк React JS. Погружение в Redux. Connect</h3>
+            
+        </>
+    );
+}
+
+export default HomePage;
